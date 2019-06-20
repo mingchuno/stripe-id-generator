4c3212e33a3120b1acb3e99080d004d96c1a02c6
mingchun.or <mingchuno@gmail.com>
2019-06-20 15:29:08 +0800

    feat: add basic logic

--- src/strip-id-generator.ts
+++ src/strip-id-generator.ts
@@ -1,4 +1,43 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import crypto from 'crypto'
+
+const ALPHA_NUM = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+export default class IdGenerator {
+  private prefixes: string[]
+  constructor(prefixes?: string | string[]) {
+    if (Array.isArray(prefixes)) {
+      this.prefixes = prefixes
+    }
+    if (typeof prefixes === 'string') {
+      this.prefixes = [prefixes]
+    }
+    this.prefixes = []
+  }
+
+  public newUid = function(len: number) {
+    const rnd = crypto.randomBytes(len)
+    const value = new Array(len)
+    const charsLength = ALPHA_NUM.length
+
+    for (let i = 0; i < len; i++) {
+      value[i] = ALPHA_NUM[rnd[i] % charsLength]
+    }
+
+    return value.join('')
+  }
+
+  public new(prefix?: string) {
+    if (!prefix) {
+      if (this.prefixes.length !== 1) {
+        throw new Error('missing prefix for id')
+      }
+      prefix = this.prefixes[0]
+    }
+
+    if (this.prefixes.length && !~this.prefixes.indexOf(prefix)) {
+      throw new Error('invalid prefix ' + prefix + ', valid: ' + this.prefixes)
+    }
+
+    return prefix + '_' + this.newUid(16)
+  }
+}
