e7f81e78f7d7a3cdad046e4e0ccdd1c27178d15e
mingchun.or <mingchuno@gmail.com>
2021-02-20 20:38:20 +0800

    fix: upgrade depns

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,6 +4,7 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import builtins from 'builtin-modules'
 
 const pkg = require('./package.json')
 
@@ -16,7 +17,7 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: builtins,
   watch: {
     include: 'src/**',
   },
